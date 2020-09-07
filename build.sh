e7b7a5a85730f4ff6de53077da5570a1159e3dda
Jonathan Bradshaw <jonathan@JonathaMacBook4.iot.internal>
2020-09-06 23:24:58 -0400

    Build changes

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-docker run --rm -ti --name hassio-builder --privileged \
+docker run --rm --privileged \
   -v $(PWD)/addon-hyperion-ng:/data \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
   homeassistant/amd64-builder \
   -t /data \
   -i hyperion-ng-addon-{arch} \
   -d bradsjm \
-  --all \
-  --test
+  --test \
+  $@
