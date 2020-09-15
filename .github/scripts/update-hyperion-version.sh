1a91788439ce255cc3cd3f8328b0374474d0b755
Jonathan Bradshaw <jonathan@JonathaMacBook4.internal>
2020-09-14 20:34:16 -0400

    Add tag and release logic

--- .github/scripts/update-hyperion-version.sh
+++ .github/scripts/update-hyperion-version.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-set -ex
+set -e
+set -o pipefail
 
 BASE=${GITHUB_WORKSPACE}/addon-hyperion-ng
 
 FILE="${BASE}/config.json"
 CURRENT="$(jq -r ".version" $FILE)"
 
+echo "::set-env name=VERSION::${CURRENT}"
+
 REPO="https://github.com/hyperion-project/hyperion.ng.git"
 RELEASE="$(git ls-remote --tags ${REPO} | cut -d/ -f3- | tail -n1)"
+
+echo "::set-env name=RELEASE::${RELEASE}"
 
 if [ "${CURRENT}" != "${RELEASE}" ]; then
     jq ".version=\"${RELEASE}\"" $FILE > $FILE.tmp
