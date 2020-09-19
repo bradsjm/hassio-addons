ae7004a4974c2f3c25dfdc1e98a92027361c0db9
Jonathan Bradshaw <jonathan@JonathaMacBook4.internal>
2020-09-18 22:42:05 -0400

    Update build script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,7 +3,6 @@ set -e
 
 docker run --rm --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
-    -v ~/.docker:/root/.docker \
     -v ${GITHUB_WORKSPACE}/addon-hyperion-ng:/data \
     homeassistant/amd64-builder \
     --target /data \
