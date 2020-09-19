312538e5519e6979f4ec71d4cf31516e411f06d3
Jonathan Bradshaw <jonathan@JonathaMacBook4.internal>
2020-09-18 22:22:56 -0400

    Update compile

---
+++ .github/scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+docker run --rm --privileged \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    -v ~/.docker:/root/.docker \
+    -v ${GITHUB_WORKSPACE}/addon-hyperion-ng:/data \
+    homeassistant/amd64-builder \
+    --target /data \
+    --no-cache \
+    --docker-user "${DOCKER_USER}" \
+    --docker-password "${DOCKER_PASSWORD}" \
+    --docker-hub-check \
+    $@
