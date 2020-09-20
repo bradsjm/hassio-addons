bc62d9d73cbf5398d84ef7eb58415f961f9328e4
Jonathan Bradshaw <jonathan@Jonathan-MacBook.internal>
2020-09-20 10:04:44 -0400

    Build fixes

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,7 +5,7 @@ ARCH=$1
 docker run --rm --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v ${GITHUB_WORKSPACE}/addon-hyperion-ng:/data \
-    homeassistant/${ARCH}-builder \
+    homeassistant/amd64-builder \
     --target /data \
     --no-cache \
     --docker-user "${DOCKER_USER}" \
