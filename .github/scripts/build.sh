b4d7baf03e31ddba70684790b743b271278770db
Jonathan Bradshaw <jonathan@Jonathan-MacBook.internal>
2020-09-20 09:33:09 -0400

    Arch Build Fixes

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
+ARCH=$1
 
 docker run --rm --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v ${GITHUB_WORKSPACE}/addon-hyperion-ng:/data \
-    homeassistant/amd64-builder \
+    homeassistant/${ARCH}-builder \
     --target /data \
     --no-cache \
     --docker-user "${DOCKER_USER}" \
     --docker-password "${DOCKER_PASSWORD}" \
     --docker-hub-check \
-    --$1
+    --${ARCH}
