dec4be375bc66d5c3cba97a075114124e195c666
Jonathan Bradshaw <jonathan@Jonathan-MacBook.internal>
2020-11-14 10:04:59 -0500

    Change defaults

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,10 +4,11 @@ ARCH=$1
 
 docker run --rm --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
-    -v ${GITHUB_WORKSPACE}/addon-hyperion-ng:/data \
+    -v ${GITHUB_WORKSPACE:-$(PWD)}/addon-hyperion-ng:/data \
     homeassistant/amd64-builder \
     --target /data \
     --docker-user "${DOCKER_USER}" \
     --docker-password "${DOCKER_PASSWORD}" \
     --docker-hub-check \
-    --${ARCH}
+    --no-latest \
+    --${ARCH:-all}
