4fbcfcf68bd9ec26ead727b7519f5028274eda8e
Jonathan Bradshaw <jb@nrgup.net>
2020-11-19 09:13:55 -0500

    Update environment variables

--- .github/scripts/update-hyperion-version.sh
+++ .github/scripts/update-hyperion-version.sh
@@ -5,12 +5,12 @@ BASE=${GITHUB_WORKSPACE}/addon-hyperion-ng
 FILE="${BASE}/config.json"
 CURRENT="$(jq -r ".version" $FILE)"
 
-echo "::set-env name=VERSION::${CURRENT}"
+echo "VERSION=${CURRENT}" >> $GITHUB_ENV
 
 REPO="https://github.com/hyperion-project/hyperion.ng.git"
 RELEASE="$(git ls-remote --tags ${REPO} | cut -d/ -f3- | tail -n1)"
 
-echo "::set-env name=RELEASE::${RELEASE}"
+echo "RELEASE=${RELEASE}" >> $GITHUB_ENV
 
 if [ "${CURRENT}" != "${RELEASE}" ]; then
     jq ".version=\"${RELEASE}\"" $FILE > $FILE.tmp
