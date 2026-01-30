7da0629430af9f6cc7ce5a3bcd447454252213ed
Jonathan Bradshaw <bradsjm@users.noreply.github.com>
2026-01-30 10:14:40 -0500

    Update build

--- .github/scripts/build-opencode.sh
+++ .github/scripts/build-opencode.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
-
 ARCH=$1
+DOCKER_ARGS=""
+
+if [ -n "${DOCKER_USER:-}" ] && [ -n "${DOCKER_PASSWORD:-}" ]; then
+    DOCKER_ARGS="--docker-user ${DOCKER_USER} --docker-password ${DOCKER_PASSWORD}"
+fi
 
 docker run --rm --privileged \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     -v ${GITHUB_WORKSPACE:-$(PWD)}/addon-opencode:/data \
     homeassistant/amd64-builder \
     --target /data \
-    --docker-user "${DOCKER_USER}" \
-    --docker-password "${DOCKER_PASSWORD}" \
+    ${DOCKER_ARGS} \
     --no-latest \
     --${ARCH:-all}
