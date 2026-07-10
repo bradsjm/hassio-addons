59f82414e6e357230d35214d556d08e827a01af6
Jonathan Bradshaw <jb@nrgup.net>
2026-07-10 15:03:06 -0400

    fix: use dynamic port syntax in webui config

--- addon-hyperion-ng/tests/test_migration.py
+++ addon-hyperion-ng/tests/test_migration.py
@@ -174,7 +174,7 @@ class HyperionMigrationTests(unittest.TestCase):
         self.assertEqual(config["arch"], ["amd64", "aarch64"])
         self.assertIs(config["host_network"], True)
         self.assertEqual(config["image"], "docker.io/bradsjm/addon-hyperion-ng")
-        self.assertEqual(config["webui"], "http://[HOST]:8090")
+        self.assertEqual(config["webui"], "http://[HOST]:[PORT:8090]")
         self.assertNotIn("ports", config)
         self.assertNotIn("ports_description", config)
 
